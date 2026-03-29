f2d10b3296f01bc7e770defe3e8c11454a9b1e66
ajay5032 <ajaykumaruk2025@gmail.com>
2026-03-29 13:33:23 +0530

    first commit

---
+++ src/com/ajay/TestDemo.java
@@ -0,0 +1,32 @@
+package com.ajay;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		byte age = 30;
+		System.out.println(age);
+		
+		short s = 25000;
+		System.out.println(s);
+		
+		int balls = 200;
+		System.out.println(balls);
+		
+		float a = 25.987f;
+		System.out.println(a);
+		
+		double price = 120.76534;
+		System.out.println(price);
+		
+		char gender = 'm';
+		System.out.println(gender);
+		
+		boolean pass = true;
+		System.out.println(pass);
+		
+		System.out.println("Hello world");
+	System.out.println("welcome to the java");
+
+	}
+
+}
---
+++ src/com/ajay/Var.java
@@ -0,0 +1,23 @@
+package com.ajay;
+
+public class Var {
+
+	public static void main(String[] args) {
+		int age = 20;
+		System.out.println(age);
+		
+		float a = 23.456f;
+		System.out.println(a);
+		
+		double price = 231.33333;
+		System.out.println(price);
+		
+		char rank ='1';
+		System.out.println(rank);
+		
+		boolean pass = true;
+		System.out.println(pass);
+
+	}
+
+}
